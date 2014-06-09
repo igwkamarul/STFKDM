bd0450a909a942f689ede2b504d305e91186ff6b
Kamarul <kamarul.ai@gmail.com>
2014-06-09 14:52:51 +0800

    change database config

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,5 @@
 <?php
-	ini_set('display_errors', 0);
-	$dbc = @mysql_connect('localhost','root','','meme') or die('ERROR CONNECTION' . mysql_error());
+	//ini_set('display_errors', 0);
+	$dbc = @mysql_connect('localhost','meme','meme','meme') or die('ERROR CONNECTION' . mysql_error());
 	mysql_select_db('meme') or die('ERROR SELECT DB' . mysql_error());
 ?>
